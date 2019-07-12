3188f183459c8f5c13c1565c532b7d32d4f71a22
Florin <fokokamga@yahoo.fr>
2019-07-12 03:11:30 +0200

    reinstall bootstrap and popper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
